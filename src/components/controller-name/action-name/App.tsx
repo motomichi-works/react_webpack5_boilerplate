94d60ef1abaea468ae4ab6dcb57a3931467d0130
motomichi <motomichi.motomichi@gmail.com>
2021-09-29 21:06:32 +0900

    eslintによる指摘箇所の修正

--- src/components/controller-name/action-name/App.tsx
+++ src/components/controller-name/action-name/App.tsx
@@ -1,11 +1,9 @@
 import * as ReactDOM from 'react-dom';
 
-export const App = function () {
-  return (
-    <div>
-      <h1>Hello React!</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>Hello React!</h1>
+  </div>
+);
 
-ReactDOM.render(<App/>, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector('#app'));
