4119c504edcb92539e30bc1afebf31665455afb8
motomichi <motomichi.motomichi@gmail.com>
2021-10-01 19:23:34 +0900

    .tsxからxxx.modules.scssをimportできるように必要なパッケージをインストールして設定

--- src/components/controller-name/action-name/App.tsx
+++ src/components/controller-name/action-name/App.tsx
@@ -1,8 +1,11 @@
 import * as ReactDOM from 'react-dom';
+import styles from './App.module.scss';
 
 const App = () => (
-  <div>
-    <h1>Hello React!</h1>
+  <div className={styles.App}>
+    <h1 className={`${styles.App__Heading} ${styles['App__Heading--hoge']}`}>
+      Hello React!
+    </h1>
   </div>
 );
 
