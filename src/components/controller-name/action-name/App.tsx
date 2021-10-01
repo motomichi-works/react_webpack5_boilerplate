55e7bfd0bd738d9feba6f687c1d4d7d18b607116
motomichi <motomichi.motomichi@gmail.com>
2021-10-02 01:51:28 +0900

    Revert ".tsxからxxx.modules.scssをimportできるように必要なパッケージをインストールして設定"
    This reverts commit 4119c504edcb92539e30bc1afebf31665455afb8.

--- src/components/controller-name/action-name/App.tsx
+++ src/components/controller-name/action-name/App.tsx
@@ -1,11 +1,8 @@
 import * as ReactDOM from 'react-dom';
-import styles from './App.module.scss';
 
 const App = () => (
-  <div className={styles.App}>
-    <h1 className={`${styles.App__Heading} ${styles['App__Heading--hoge']}`}>
-      Hello React!
-    </h1>
+  <div>
+    <h1>Hello React!</h1>
   </div>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,3 @@
-// [定数] webpack の出力オプションを指定します
-// 'production' か 'development' を指定
-const MODE = 'production';
-
-// ソースマップの利用有無(productionのときはソースマップを利用しない)
-const enabledSourceMap = MODE === 'development';
-
 module.exports = {
   mode: 'development',
   entry: {
@@ -14,7 +7,7 @@ module.exports = {
   output: {
     path: `${__dirname}/public/javascripts`,
     filename: (arg) => {
-      return '[name].bundle.js';
+      return '[name].bundle.js'
     },
   },
   module: {
@@ -22,47 +15,14 @@ module.exports = {
       // 拡張子 .ts もしくは .tsx の場合
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-      },
-      // 拡張子 .scss の場合
-      {
-        test: /\.scss/,
-        use: [
-          // linkタグに出力する機能
-          {
-            loader: 'style-loader',
-          },
-          // CSSをバンドルするための機能
-          {
-            loader: 'css-loader',
-            options: {
-              // オプションでCSS内のurl()メソッドを取り込む
-              url: true,
-              // ソースマップの利用有無
-              sourceMap: enabledSourceMap,
-
-              // 0 => no loaders (default);
-              // 1 => postcss-loader;
-              // 2 => postcss-loader, sass-loader
-              importLoaders: 2,
-            },
-          },
-          // Sassをバンドルするための機能
-          {
-            loader: 'sass-loader',
-            options: {
-              // ソースマップの利用有無
-              sourceMap: enabledSourceMap,
-            },
-          },
-        ],
-      },
-    ],
+        use: "ts-loader"
+      }
+    ]
   },
   resolve: {
     // import時に省略できる拡張子を設定
-    extensions: ['.ts', '.tsx', '.js', '.json'],
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
   // ES5(IE11等)向けの指定（webpack 5以上で必要）
-  target: ['web', 'es5'],
+  target: ["web", "es5"],
 };
